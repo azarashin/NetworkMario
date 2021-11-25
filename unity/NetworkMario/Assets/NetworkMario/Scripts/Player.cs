697747f2afe3b62c8a849a07825e9140009f0436
azarashin <azarashin1979@gmail.com>
2021-11-26 02:06:51 +0900

    床の上にいるときだけジャンプするように改良。デバッグ用のラインも表示するようにした。

--- unity/NetworkMario/Assets/NetworkMario/Scripts/Player.cs
+++ unity/NetworkMario/Assets/NetworkMario/Scripts/Player.cs
@@ -16,13 +16,15 @@ public class Player : MonoBehaviour
     [SerializeField]
     float _jumpImpulse = 10.0f;
 
-    Rigidbody2D _rigidBody; 
+    Rigidbody2D _rigidBody;
+    CapsuleCollider2D _collider; 
 
     // Start is called before the first frame update
     void Start()
     {
         _animator.speed = 0.0f;
-        _rigidBody = GetComponent<Rigidbody2D>(); 
+        _rigidBody = GetComponent<Rigidbody2D>();
+        _collider = GetComponent<CapsuleCollider2D>(); 
     }
 
     // Update is called once per frame
@@ -44,9 +46,20 @@ public class Player : MonoBehaviour
         {
             _animator.speed = 0.0f;
         }
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W) && IsOnFloor())
         {
             _rigidBody.AddForce(Vector2.up * _jumpImpulse, ForceMode2D.Impulse);
         }
+    }
+
+    bool IsOnFloor()
+    {
+        int block_layer = 6;
+        int mask = 1 << block_layer; 
+        float margin = 0.01f;
+        float threshold_speed = 1.0f;
+        Debug.DrawLine(transform.position, transform.position + Vector3.down * (_collider.size.y * 0.5f + margin));
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, _collider.size.y * 0.5f + margin, mask);
+        return (hit && Mathf.Abs(_rigidBody.velocity.y) < threshold_speed); 
     }
 }
