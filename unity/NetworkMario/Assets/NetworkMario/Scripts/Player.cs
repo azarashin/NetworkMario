bac7ccc78e47ddf176891aab791a2b1fde37df36
azarashin <azarashin1979@gmail.com>
2021-11-26 01:17:53 +0900

    ボタンを押したらアニメーションする機能を追加。

--- unity/NetworkMario/Assets/NetworkMario/Scripts/Player.cs
+++ unity/NetworkMario/Assets/NetworkMario/Scripts/Player.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField]
+    Animator _animator;
+
+    [SerializeField]
+    float _speed = 1.0f; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _animator.speed = 0.0f; 
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if(Input.GetKey(KeyCode.A))
+        {
+            _animator.speed = _speed;
+            _animator.SetFloat("dir", -1.0f); 
+        } else if (Input.GetKey(KeyCode.D))
+        {
+            _animator.speed = _speed;
+            _animator.SetFloat("dir", 1.0f);
+        } else {
+            _animator.speed = 0.0f;
+        }
     }
 }
